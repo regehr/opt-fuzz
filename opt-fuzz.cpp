ac4aa118fa4ef24e1bb47eec00d0345092336dc3
John Regehr <regehr@cs.utah.edu>
2015-05-10 11:47:41 -0600

    still a forkbomb, but now properly dumping each program to its file

--- opt-fuzz.cpp
+++ opt-fuzz.cpp
@@ -37,25 +37,35 @@
 #include <vector>
 using namespace llvm;
 
-static const int W = 8; // width
-static const int N = 1; // number of instructions to generate
-static const int MaxArgs = 20;
+static const int W = 3; // width
+static const int N = 2; // number of instructions to generate
+static const int MaxArgs = N + 2;
 
 static cl::opt<std::string> OutputFilename("o",
                                            cl::desc("Override output filename"),
                                            cl::value_desc("filename"));
 
+static cl::opt<bool> All("all", cl::desc("Generate all programs"),
+                         cl::init(false));
+
 static std::vector<int> Choices;
 
 static int Choose(int n) {
-  // return rand() % n;
-  for (int i=0; i<n; ++i) {
-    int ret = ::fork();
-    assert(ret != -1);
-    if (ret==0)
-      return i;
+  if (All) {
+    for (int i = 0; i < n; ++i) {
+      int ret = ::fork();
+      assert(ret != -1);
+      if (ret == 0) {
+        Choices.push_back(i);
+        return i;
+      }
+    }
+    exit(0);
+  } else {
+    int i = rand() % n;
+    Choices.push_back(i);
+    return i;
   }
-  exit(0);
 }
 
 static std::vector<Type *> ArgsTy;
@@ -79,20 +89,36 @@ static Value *getVal() {
     return Vals.at(Choose(Vals.size()));
   case 1:
     // return a constant
-    return ConstantInt::get(*C, APInt(W, Choose(1<<W)));
+    return ConstantInt::get(*C, APInt(W, Choose(1 << W)));
   case 2: {
     // make a new instruction
     --budget;
     Value *V;
     switch (Choose(8)) {
-    case 0: V = builder->CreateAdd(getVal(), getVal()); break;
-    case 1: V = builder->CreateSub(getVal(), getVal()); break;
-    case 2: V = builder->CreateMul(getVal(), getVal()); break;
-    case 3: V = builder->CreateSDiv(getVal(), getVal()); break;
-    case 4: V = builder->CreateUDiv(getVal(), getVal()); break;
-    case 5: V = builder->CreateAnd(getVal(), getVal()); break;
-    case 6: V = builder->CreateOr(getVal(), getVal()); break;
-    case 7: V = builder->CreateXor(getVal(), getVal()); break;
+    case 0:
+      V = builder->CreateAdd(getVal(), getVal());
+      break;
+    case 1:
+      V = builder->CreateSub(getVal(), getVal());
+      break;
+    case 2:
+      V = builder->CreateMul(getVal(), getVal());
+      break;
+    case 3:
+      V = builder->CreateSDiv(getVal(), getVal());
+      break;
+    case 4:
+      V = builder->CreateUDiv(getVal(), getVal());
+      break;
+    case 5:
+      V = builder->CreateAnd(getVal(), getVal());
+      break;
+    case 6:
+      V = builder->CreateOr(getVal(), getVal());
+      break;
+    case 7:
+      V = builder->CreateXor(getVal(), getVal());
+      break;
     }
     Vals.push_back(V);
     return V;
@@ -109,11 +135,11 @@ int main(int argc, char **argv) {
 
   Module *M = new Module("/tmp/autogen.bc", getGlobalContext());
   C = &M->getContext();
-  for (int i=0; i<MaxArgs; ++i)
+  for (int i = 0; i < MaxArgs; ++i)
     ArgsTy.push_back(IntegerType::getIntNTy(*C, W));
   FunctionType *FuncTy = FunctionType::get(Type::getIntNTy(*C, W), ArgsTy, 0);
   Function *F =
-    Function::Create(FuncTy, GlobalValue::ExternalLinkage, "autogen", M);
+      Function::Create(FuncTy, GlobalValue::ExternalLinkage, "autogen", M);
   NextArg = F->arg_begin();
   builder = new IRBuilder<true, NoFolder>(BasicBlock::Create(*C, "", F));
 
@@ -122,6 +148,16 @@ int main(int argc, char **argv) {
 
   if (OutputFilename.empty())
     OutputFilename = "-";
+
+  std::string ChoiceStr = "";
+  for (std::vector<int>::iterator it = Choices.begin(); it != Choices.end();
+       ++it)
+    ChoiceStr += std::to_string(*it) + "_";
+  ChoiceStr.erase(ChoiceStr.end()-1);
+
+  if (All)
+    OutputFilename = ChoiceStr + ".ll";
+
   std::unique_ptr<tool_output_file> Out;
   std::error_code EC;
   Out.reset(new tool_output_file(OutputFilename, EC, sys::fs::F_None));
@@ -129,6 +165,8 @@ int main(int argc, char **argv) {
     errs() << EC.message() << '\n';
     return 1;
   }
+
+  Out->os() << "; Choices: " << ChoiceStr << "\n";
 
   legacy::PassManager Passes;
   Passes.add(createVerifierPass());

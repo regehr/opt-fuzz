2ed0d3137ad960833ca4f352374208405f34046a
John Regehr <regehr@cs.utah.edu>
2019-10-27 22:53:00 -0600

    foo

--- scripts/test-llvm-backends/check-file.pl
+++ scripts/test-llvm-backends/check-file.pl
@@ -10,36 +10,57 @@ my $WIDTH = 32;
 my $inf = $ARGV[0];
 die unless -f $inf;
 
-my @suffixes = (".bc", ".ll");
-my ($base, $dirs, $suffix) = fileparse($inf, @suffixes);
-die unless defined $suffix;
+my %suffixes = (".bc" => 1,
+                ".ll" => 1);
+my ($base, $dirs, $suffix) = fileparse($inf, keys %suffixes);
+die "expected LLVM IR" unless exists $suffixes{$suffix};
 $base = $dirs . $base;
 
-print "$base\n";
-exit 0;
-
 # opt-fuzz emits too many arguments, get rid of unneeded ones
-system "opt -strip $inf -S -o - | ${OPTFUZZ}/scripts/test-llvm-backends/unused-arg-elimination.pl | opt -strip -S -o ${BASE}-stripped.ll";
+system "opt -strip $inf -S -o - | ${OPTFUZZ}/scripts/test-llvm-backends/unused-arg-elimination.pl | opt -strip -S -o ${base}-stripped.ll";
 
 # IR -> object code
-system "clang -c -O ${BASE}-stripped.ll -o ${BASE}.o";
+system "clang -c -O ${base}-stripped.ll -o ${base}.o";
 
 ####################################################
 # object code -> IR, run one of these
 
-system "/home/regehr/retdec-install/bin/retdec-decompiler.py ${BASE}.o";
-system "mv all.o.ll all-decomp.ll";
+# ANVILL
+if (1) {
+    open my $INF, "objdump -d ${base}.o |" or die;
+    my $bytes = "";
+    while (my $line = <$INF>) {
+        if ($line =~ /[0-9a-f]+:\s+(([0-9a-f][0-9a-f] )+)\s+[a-zA-Z]/) {
+            my $asm = $1;
+            $asm =~ s/ //g;
+            $bytes .= $asm;
+        }
+    }
+    close $INF;
+    print "$bytes\n";
+}
 
-system "reopt --header=protos.h --llvm ${BASE}.o > ${BASE}-decomp.ll";
+# MCTOLL
 
-system "/home/regehr/mctoll/llvm-project/build/bin/llvm-mctoll ${BASE}.o";
+# RETDEC
+if (0) {
+    system "/home/regehr/retdec-install/bin/retdec-decompiler.py ${base}.o";
+    system "mv all.o.ll all-decomp.ll";
+}
+
+# REOPT
+if (0) {
+    system "reopt --header=protos.h --llvm ${base}.o > ${base}-decomp.ll";
+}
 
 ####################################################
+
+exit 0;
 
 # ABI allows unused bits of return to be trashed; add an instruction
 # masking these bits off. also strip out whatever register names the
 # decompiler decided to use.
-system "~/alive2/scripts/test-llvm-backends/maskret.pl $WIDTH < ${BASE}-decomp.ll | opt -strip -S -o ${BASE}-decomp2.ll";
+system "~/alive2/scripts/test-llvm-backends/maskret.pl $WIDTH < ${base}-decomp.ll | opt -strip -S -o ${base}-decomp2.ll";
 
 # translation validation
-system "~/alive2/build/alive-tv ${BASE}-stripped.ll ${BASE}-decomp2.ll --disable-poison-input --disable-undef-input";
+system "~/alive2/build/alive-tv ${base}-stripped.ll ${base}-decomp2.ll --disable-poison-input --disable-undef-input";

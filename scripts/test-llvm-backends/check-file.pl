8176e3612dc5ff4aa260377006635a04af472b7e
John Regehr <regehr@cs.utah.edu>
2019-10-27 22:14:20 -0600

    foo

--- scripts/test-llvm-backends/check-file.pl
+++ scripts/test-llvm-backends/check-file.pl
@@ -1,35 +1,45 @@
-set -e
+#!/usr/bin/perl -w
 
-OPTFUZZ=$HOME/opt-fuzz
+use strict;
+use autodie;
+use File::Basename;
 
-# filename
-BASE=all
+my $OPTFUZZ=$ENV{"HOME"}."/opt-fuzz";
+my $WIDTH = 32;
 
-# this has to be the native width used by opt-fuzz
-WIDTH=32
+my $inf = $ARGV[0];
+die unless -f $inf;
+
+my @suffixes = (".bc", ".ll");
+my ($base, $dirs, $suffix) = fileparse($inf, @suffixes);
+die unless defined $suffix;
+$base = $dirs . $base;
+
+print "$base\n";
+exit 0;
 
 # opt-fuzz emits too many arguments, get rid of unneeded ones
-opt -strip ${BASE}.ll -S -o - | ${OPTFUZZ}/scripts/test-llvm-backends/unused-arg-elimination.pl | opt -strip -S -o ${BASE}-stripped.ll
+system "opt -strip $inf -S -o - | ${OPTFUZZ}/scripts/test-llvm-backends/unused-arg-elimination.pl | opt -strip -S -o ${BASE}-stripped.ll";
 
 # IR -> object code
-clang -c -O ${BASE}-stripped.ll -o ${BASE}.o
+system "clang -c -O ${BASE}-stripped.ll -o ${BASE}.o";
 
 ####################################################
 # object code -> IR, run one of these
 
-/home/regehr/retdec-install/bin/retdec-decompiler.py ${BASE}.o
-mv all.o.ll all-decomp.ll
+system "/home/regehr/retdec-install/bin/retdec-decompiler.py ${BASE}.o";
+system "mv all.o.ll all-decomp.ll";
 
-reopt --header=protos.h --llvm ${BASE}.o > ${BASE}-decomp.ll
+system "reopt --header=protos.h --llvm ${BASE}.o > ${BASE}-decomp.ll";
 
-/home/regehr/mctoll/llvm-project/build/bin/llvm-mctoll ${BASE}.o
+system "/home/regehr/mctoll/llvm-project/build/bin/llvm-mctoll ${BASE}.o";
 
 ####################################################
 
 # ABI allows unused bits of return to be trashed; add an instruction
 # masking these bits off. also strip out whatever register names the
 # decompiler decided to use.
-~/alive2/scripts/test-llvm-backends/maskret.pl $WIDTH < ${BASE}-decomp.ll | opt -strip -S -o ${BASE}-decomp2.ll
+system "~/alive2/scripts/test-llvm-backends/maskret.pl $WIDTH < ${BASE}-decomp.ll | opt -strip -S -o ${BASE}-decomp2.ll";
 
 # translation validation
-~/alive2/build/alive-tv ${BASE}-stripped.ll ${BASE}-decomp2.ll --disable-poison-input --disable-undef-input
+system "~/alive2/build/alive-tv ${BASE}-stripped.ll ${BASE}-decomp2.ll --disable-poison-input --disable-undef-input";

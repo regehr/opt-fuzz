5f9e604d0dccf53f1683116257a4c5f1446f888d
John Regehr <regehr@cs.utah.edu>
2019-10-30 22:30:12 -0600

    foo

--- scripts/run-for-64bit-abi.sh
+++ scripts/run-for-64bit-abi.sh
@@ -1,15 +1,15 @@
 N=1
 OPTFUZZ=/home/regehr/opt-fuzz/build/opt-fuzz
-ARGS='--cores=10 --fewconsts --promote=64 --one-func-per-file'
+ARGS='--cores=10 --fewconsts --promote=64 --one-func-per-file --use-intrinsics=false'
 
-$OPTFUZZ $ARGS --width=8 --num-insns=$N --base="w8_"
-$OPTFUZZ $ARGS --width=16 --num-insns=$N --base="w16_"
-$OPTFUZZ $ARGS --width=32 --num-insns=$N --base="w32_"
+#$OPTFUZZ $ARGS --width=8 --num-insns=$N --base="w8_"
+#$OPTFUZZ $ARGS --width=16 --num-insns=$N --base="w16_"
+#$OPTFUZZ $ARGS --width=32 --num-insns=$N --base="w32_"
 $OPTFUZZ $ARGS --width=64 --num-insns=$N --base="w64_"
-$OPTFUZZ $ARGS --width=8 --num-insns=$N --geni1 --base="w8r1_"
-$OPTFUZZ $ARGS --width=16 --num-insns=$N --geni1 --base="w16r1_"
-$OPTFUZZ $ARGS --width=32 --num-insns=$N --geni1 --base="w32r1_"
-$OPTFUZZ $ARGS --width=64 --num-insns=$N --geni1 --base="w64r1_"
+#$OPTFUZZ $ARGS --width=8 --num-insns=$N --geni1 --base="w8r1_"
+#$OPTFUZZ $ARGS --width=16 --num-insns=$N --geni1 --base="w16r1_"
+#$OPTFUZZ $ARGS --width=32 --num-insns=$N --geni1 --base="w32r1_"
+#$OPTFUZZ $ARGS --width=64 --num-insns=$N --geni1 --base="w64r1_"
 
 # add a version with args/rets in memory
 
